16ea5da2974e92782527e48d83be7d8b20c41000
Socheat Sok <alex@socheat.net>
2020-04-14 20:04:38 +0700

    add my custom theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import socheat from './themes/socheat'
+
+addons.setConfig({
+    theme: socheat
+});
---
+++ .storybook/themes/socheat.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming/create';
+import { themes } from '@storybook/theming';
+
+export default create({
+    brandTitle: 'My custom storybook',
+    brandUrl: 'https://example.com',
+    brandImage: '/images/socheat.png',
+
+    ...themes.dark
+}
+)
